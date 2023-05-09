b75d56aaeafda49f18020aead1793bd17750ad02
Felipe Pinto <felipe14062000@gmail.com>
2023-05-09 16:01:29 -0300

    feat: criação da função main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,14 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.importer_manager import importer_manager
+
+
 def main():
-    pass
+    try:
+        _, path, type = sys.argv
+        importer = importer_manager(path)
+
+        result = InventoryRefactor(importer).import_data(path, type)
+        sys.stdout.write(result)
+    except ValueError:
+        print("Verifique os argumentos", file=sys.stderr)
